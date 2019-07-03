1687100df3af0ef82e4352c15689644cab84f882
Riz <goyalrizul1997@gmail.com>
2019-07-03 22:26:58 +0530

    a stupid game

---
+++ stupid_game.py
@@ -0,0 +1,13 @@
+#msg = input("Say Something")
+# msg = ""
+# while msg != "Stop Copying Me":
+#     msg = input("Say Something")
+#     print(msg)
+
+# print("Fine, You Win")
+
+msg = input("Say Something")
+while msg != "Stop Copying Me":
+    print(msg)
+    msg = input()
+print("Fine, You Win")
