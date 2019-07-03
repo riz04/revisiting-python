57c0415c9b0b24d6a36d131e28375c8fefc5ac1d
Riz <goyalrizul1997@gmail.com>
2019-07-03 22:27:58 +0530

    another game

---
+++ Unlucky.py
@@ -0,0 +1,9 @@
+
+for x in range(1,21):
+    if (x ==4 or x==13):
+        print(f"{x} : x is unlucky")
+    elif (x % 2 == 0):
+        print(f"{x} : x is even")
+    else:
+        print(f"{x} : x is odd")
+
